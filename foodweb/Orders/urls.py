c705ec82b28cf97702a31f6667b79fd4106ed4a8
Akshit Sahore <akshitsahore2827@gmail.com>
2025-05-10 01:14:15 +0530

    Delete foodweb directory

--- foodweb/Orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name='orders'
-
-urlpatterns = [
-    path('',views.order_list,name="order_list"),
-    path('create/',views.create_order,name="new_order")
-]
--- foodweb/foodweb/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-#creating views
-def Homepage(request):
-    return render(request,'home.html')
-
-def About(request):
-    return render(request,'about.html')
